4e380b2fef000a8cbabaaeb2eb0e5b89cb820706
UsatiyNyan <us4tiyny4n@gmail.com>
2024-03-09 15:25:23 +0100

    iterative export

--- cph/cli/parse_power_log.py
+++ cph/cli/parse_power_log.py
@@ -2,7 +2,7 @@ import time
 import logging
 from pathlib import Path
 
-from hslog import packets, LogParser
+from hslog import LogParser
 
 from cph.utils.logging import make_logger
 from cph.game import power_log, exporter
@@ -10,10 +10,13 @@ from cph.game import power_log, exporter
 
 def main():
     logger = make_logger('parse_power_log', logging.DEBUG)
+
     power_log_path: Path | None = None
     offset = 0
     parser = LogParser()
-    prev_game: packets.PacketTree | None = None
+
+    game_state_exporter: exporter.GameStateExporter | None = None
+    packet_offset = 0
 
     def read_line(line: str) -> None:
         parser.read_line(line)
@@ -22,15 +25,8 @@ def main():
         power_log_path, offset = \
             power_log.handle_lines_once(read_line, power_log_path, offset)
 
-        # preparation for queueing / incremental exporting
-        # would not read the packets completely
-        # but rather as they come
-        current_game: packets.PacketTree = parser.games[-1]
-        game_state_exporter = exporter.GameStateExporter(parser.player_manager, logger)
-        for packet in current_game:
-            game_state_exporter.export_packet(packet)
-        game_state_exporter.flush()
-
+        game_state_exporter, packet_offset = exporter.handle_packets(
+            parser, logger, game_state_exporter, packet_offset)
         time.sleep(1)
 
 
--- cph/game/exporter.py
+++ cph/game/exporter.py
@@ -1,46 +1,51 @@
 import typing as t
 import logging
-from hearthstone import entities, enums
-from hslog import packets, player, export, exceptions
+import hearthstone.entities
+import hearthstone.enums
+import hslog.packets
+import hslog.player
+import hslog.export
+import hslog.exceptions
+import hslog.parser
 
 
-class GameStateExporter(export.BaseExporter):
+class GameStateExporter(hslog.export.BaseExporter):
     def __init__(self,
-                 manager: player.PlayerManager,
+                 manager: hslog.player.PlayerManager,
                  logger: logging.Logger):
         super().__init__(packet_tree=None)  # export() is not used
         self.manager = manager
         self.logger = logger
-        self.game: entities.Game | None = None
+        self.game: hearthstone.entities.Game | None = None
 
-    def _find_entity(self, entity_id: int) -> entities.Entity | None:
+    def _find_entity(self, entity_id: int) -> hearthstone.entities.Entity | None:
         if self.game is None:
             self.logger.warning('find_entity: no game')
             return None
         try:
             return self.game.find_entity_by_id(entity_id)
-        except exceptions.MissingPlayerData:
+        except hslog.exceptions.MissingPlayerData:
             return None
 
-    def handle_create_game(self, packet: packets.CreateGame):
+    def handle_create_game(self, packet: hslog.packets.CreateGame):
         self.logger.debug(f'create game: {packet.entity=}')
-        self.game = entities.Game(packet.entity)
+        self.game = hearthstone.entities.Game(packet.entity)
         self.game.create(packet.tags)
         for player_packet in packet.players:
             self.export_packet(player_packet)
 
-    def handle_player(self, packet: packets.CreateGame.Player):
+    def handle_player(self, packet: hslog.packets.CreateGame.Player):
         if self.game is None:
             self.logger.warning('player: no game')
             return
 
-        entity_id = player.coerce_to_entity_id(packet.entity)
+        entity_id = hslog.player.coerce_to_entity_id(packet.entity)
         player_entity = self.manager.get_player_by_entity_id(int(entity_id))
         if player_entity is not None:
             packet.name = player_entity.name
         self.logger.debug(f'player: {entity_id=} name={packet.name}')
 
-        entity = entities.Player(
+        entity = hearthstone.entities.Player(
             entity_id,
             packet.player_id,
             packet.hi,
@@ -49,16 +54,16 @@ class GameStateExporter(export.BaseExporter):
         )
         entity.tags = dict(packet.tags)
         entity.initial_hero_entity_id = entity.tags.get(
-            enums.GameTag.HERO_ENTITY, 0)
+            hearthstone.enums.GameTag.HERO_ENTITY, 0)
         self.game.register_entity(entity)
 
-    def handle_block(self, packet: packets.Block):
-        if packet.type == enums.BlockType.GAME_RESET and self.game is not None:
+    def handle_block(self, packet: hslog.packets.Block):
+        if packet.type == hearthstone.enums.BlockType.GAME_RESET and self.game is not None:
             self.logger.debug('game reset')
             self.game.reset()
         super().handle_block(packet)
 
-    def handle_full_entity(self, packet: packets.FullEntity):
+    def handle_full_entity(self, packet: hslog.packets.FullEntity):
         if self.game is None:
             self.logger.warning('full_enitity: no game')
             return
@@ -68,11 +73,11 @@ class GameStateExporter(export.BaseExporter):
             self.logger.warning(f'full_entity: {entity_id} already exists')
             return
 
-        entity = entities.Card(int(entity_id), packet.card_id)
+        entity = hearthstone.entities.Card(int(entity_id), packet.card_id)
         entity.tags = dict(packet.tags)
         self.game.register_entity(entity)
 
-    def handle_hide_entity(self, packet: packets.HideEntity):
+    def handle_hide_entity(self, packet: hslog.packets.HideEntity):
         if self.game is None:
             self.logger.warning('hide_entity: no game')
             return
@@ -80,10 +85,10 @@ class GameStateExporter(export.BaseExporter):
         if entity is None:
             self.logger.warning('hide_entity: entity not found')
             return
-        card = t.cast(entities.Card, entity)
+        card = t.cast(hearthstone.entities.Card, entity)
         card.hide()
 
-    def handle_show_entity(self, packet: packets.ShowEntity):
+    def handle_show_entity(self, packet: hslog.packets.ShowEntity):
         if self.game is None:
             self.logger.warning('show_entity: no game')
             return
@@ -91,10 +96,10 @@ class GameStateExporter(export.BaseExporter):
         if entity is None:
             self.logger.warning('show_entity: entity not found')
             return
-        card = t.cast(entities.Card, entity)
+        card = t.cast(hearthstone.entities.Card, entity)
         card.reveal(packet.card_id, dict(packet.tags))
 
-    def handle_change_entity(self, packet: packets.ChangeEntity):
+    def handle_change_entity(self, packet: hslog.packets.ChangeEntity):
         if self.game is None:
             self.logger.warning('change_entity: no game')
             return
@@ -102,7 +107,7 @@ class GameStateExporter(export.BaseExporter):
         if entity is None:
             self.logger.warning('change_entity: entity not found')
             return
-        card = t.cast(entities.Card, entity)
+        card = t.cast(hearthstone.entities.Card, entity)
         if card.card_id is None:
             self.logger.warning(
                 'change_entity: '
@@ -110,40 +115,45 @@ class GameStateExporter(export.BaseExporter):
             return
         card.change(packet.card_id, dict(packet.tags))
 
-    def handle_tag_change(self, packet: packets.TagChange):
+    def handle_tag_change(self, packet: hslog.packets.TagChange):
         if self.game is None:
             self.logger.warning('tag_change: no game')
             return
-        entity_id = player.coerce_to_entity_id(packet.entity)
+        entity_id = hslog.player.coerce_to_entity_id(packet.entity)
         entity = self._find_entity(int(entity_id))
         if entity is None:
             self.logger.warning('tag_change: entity not found')
             return
-        card = t.cast(entities.Card, entity)
+        card = t.cast(hearthstone.entities.Card, entity)
         card.tag_change(packet.tag, packet.value)
 
-    def handle_metadata(self, packet: packets.MetaData):
+    def handle_metadata(self, packet: hslog.packets.MetaData):
         pass
 
-    def handle_choices(self, packet: packets.Choices):
-        self.logger.debug(f'choices: {packet.player=}')
+    def handle_choices(self, packet: hslog.packets.Choices):
+        entity_id = hslog.player.coerce_to_entity_id(packet.entity)
+        player_entity = self.manager.get_player_by_entity_id(int(entity_id))
+        player_name = player_entity.name if player_entity is not None else None
+        first_player = self.manager.first_player
+        is_first_player = first_player is not None and first_player.entity_id == entity_id
+        self.logger.debug(f'choices: name: {player_name} is first: {is_first_player}')
         for choice in packet.choices:
             entity = self._find_entity(choice)
             self.logger.debug(f'choice: {entity}')
             # TODO: handle COIN <- second player
             # TODO: handle mulligan -> friendly player
 
-    def handle_send_choices(self, packet: packets.SendChoices):
+    def handle_send_choices(self, packet: hslog.packets.SendChoices):
         pass
 
-    def handle_chosen_entities(self, packet: packets.ChosenEntities):
+    def handle_chosen_entities(self, packet: hslog.packets.ChosenEntities):
         pass
 
-    def handle_options(self, packet: packets.Options):
+    def handle_options(self, packet: hslog.packets.Options):
         for option in packet.options:
             self.export_packet(option)
 
-    def handle_option(self, packet: packets.Option):
+    def handle_option(self, packet: hslog.packets.Option):
         if self.game is None:
             self.logger.warning('option: no game')
             return
@@ -155,24 +165,56 @@ class GameStateExporter(export.BaseExporter):
         for target in packet.options:
             target_entity = None if target.entity is None \
                 else self._find_entity(target.entity)
-            self.logger.debug(f'\ttarget: {target_entity} error={target.error}')
+            self.logger.debug(
+                f'\ttarget: {target_entity} error={target.error}')
 
-    def handle_send_option(self, packet: packets.SendOption):
+    def handle_send_option(self, packet: hslog.packets.SendOption):
         pass
 
-    def handle_reset_game(self, packet: packets.ResetGame):
+    def handle_reset_game(self, packet: hslog.packets.ResetGame):
         pass
 
-    def handle_sub_spell(self, packet: packets.SubSpell):
+    def handle_sub_spell(self, packet: hslog.packets.SubSpell):
         super().handle_sub_spell(packet)
 
     def handle_cached_tag_for_dormant_change(
-            self, packet: packets.CachedTagForDormantChange
+            self, packet: hslog.packets.CachedTagForDormantChange
     ):
         pass
 
-    def handle_vo_spell(self, packet: packets.VOSpell):
+    def handle_vo_spell(self, packet: hslog.packets.VOSpell):
         pass
 
-    def handle_shuffle_deck(self, packet: packets.ShuffleDeck):
+    def handle_shuffle_deck(self, packet: hslog.packets.ShuffleDeck):
         pass
+
+
+def extract_last_game(parser: hslog.LogParser) -> t.Tuple[hslog.packets.PacketTree | None, bool]:
+    if len(parser.games) == 0:
+        return None, False
+
+    is_new_game = len(parser.games) > 1
+    while len(parser.games) > 1:
+        parser.games.pop(0)
+    return parser.games[0], is_new_game
+
+
+def handle_packets(parser: hslog.LogParser,
+                   logger: logging.Logger,
+                   exporter: GameStateExporter | None,
+                   packet_offset: int) \
+        -> t.Tuple[GameStateExporter | None, int]:
+    current_game, is_new_game = extract_last_game(parser)
+    if current_game is None:
+        return exporter, packet_offset
+
+    exporter = exporter \
+        if not is_new_game and exporter is not None\
+        else GameStateExporter(parser.player_manager, logger)
+    packet_offset = packet_offset if not is_new_game else 0
+
+    for packet in current_game.packets[packet_offset:]:
+        exporter.export_packet(packet)
+    exporter.flush()
+
+    return exporter, len(current_game.packets)
