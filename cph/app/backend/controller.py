7ea65db7615e485d1fbcc780c03537148a95f721
UsatiyNyan <us4tiyny4n@gmail.com>
2024-03-09 22:55:56 +0100

    parser timer

--- cph/app/backend/controller.py
+++ cph/app/backend/controller.py
@@ -1,6 +1,6 @@
 import logging
 
-from PySide6.QtCore import QObject, Property
+from PySide6.QtCore import QObject, Property, QTimer
 from PySide6.QtQml import qmlRegisterType
 
 from cph.utils.logging import make_logger
@@ -20,6 +20,11 @@ class Controller(QObject):
         self._logger = make_logger('app', logging.DEBUG)
         self._handler = VoteHandler(self._voteController, self._logger)
         self._parser = PowerLogParser(self._handler, self._logger)
+
+        self._timer = QTimer(self)
+        self._timer.timeout.connect(self._parser.parse_once)
+        self._timer.setSingleShot(False)
+        self._timer.start(1000)
 
     @Property(VoteController, constant=True)
     def voteController(self):
